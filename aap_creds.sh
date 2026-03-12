b273ac3e1bf564acd8bbf20e2b8675d58253f71a
fvzwieten <fvanzwie@redhat.com>
2026-03-12 10:29:28 +0100

    Delete aap_creds.sh

--- aap_creds.sh
+++
@@ -1,6 +0,0 @@
-# /bin/bash
-# for running outside of AAP
-export CONTROLLER_HOSTNAME=https://aap-aap.apps.cluster-6crkd-1.dynamic.redhatworkshops.io
-export CONTROLLER_USERNAME=admin
-export CONTROLLER_PASSWORD=
-export CONTROLLER_VERIFY_SSL=false
