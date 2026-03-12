091c82497bd98084db2fbef25e204c95b2f63092
Fred van Zwieten <outlier.bezel0w@icloud.com>
2026-03-12 09:51:06 +0100

    Move to AAP based

--- aap_creds.sh
+++ aap_creds.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 # for running outside of AAP
-export CONTROLLER_HOSTNAME=<aap url>
-export CONTROLLER_USERNAME=<username>
-export CONTROLLER_PASSWORD=<password>
+export CONTROLLER_HOSTNAME=https://aap-aap.apps.cluster-6crkd-1.dynamic.redhatworkshops.io
+export CONTROLLER_USERNAME=admin
+export CONTROLLER_PASSWORD=MjkyMTYx_1
 export CONTROLLER_VERIFY_SSL=false
